7330dec7f6944c6b90254e6af7cd146a532236c6
AhmedMaher197 <ahmed.maher66632@gmail.com>
2025-02-21 21:39:55 +0200

    Implement Task4

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -18,6 +18,9 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mtx);
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();
 }
 
 
@@ -64,6 +67,7 @@ void TrafficLight::cycleThroughPhases()
         {
             _currentPhase = (_currentPhase == TrafficLightPhase::red) ? TrafficLightPhase::green : TrafficLightPhase::red;
             ///TODO: Add _messageQueue Send.
+            _messageQueue.send(std::move(getCurrentPhase()));
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
